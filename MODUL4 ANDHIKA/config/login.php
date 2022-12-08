9133bc40813d86e6a5983e554d83c80c1ffee24b
andhikaihs <andhikaihs@student.telkomuniversity.ac.id>
2022-12-08 20:15:31 +0700

    update

--- MODUL4 ANDHIKA/config/login.php
+++ MODUL4 ANDHIKA/config/login.php
@@ -1,31 +1,33 @@
 <?php 
 include ("connector.php");
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+if(isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-$emailCheck = "SELECT * FROM user_andhika WHERE email = '$email'";
-$check = mysqli_query($conn, $emailCheck);
-session_start();
-if(mysqli_num_rows($check)==1){
-    $result = mysqli_fetch_assoc($check);   
+    $emailCheck = "SELECT * FROM user_andhika WHERE email = '$email'";
+    $check = mysqli_query($conn, $emailCheck);
+    session_start();
+    if(mysqli_num_rows($check)==1){
+        $result = mysqli_fetch_assoc($check);   
 
-    if($password == $result['password']){
-        $_SESSION['id'] = $result['id'];
-        $_SESSION['nama'] = $result['nama'];
-        $_SESSION['email'] = $result['email'];
-        $_SESSION['no_hp'] = $result['no_hp'];
+        if($password == $result['password']){
+            $_SESSION['id'] = $result['id'];
+            $_SESSION['nama'] = $result['nama'];
+            $_SESSION['email'] = $result['email'];
+            $_SESSION['no_hp'] = $result['no_hp'];
 
-        $_SESSION['message'] = 'Berhasil Login';
-        header("location:../index.php");
-    } else { ?>
-        <div class="alert alert-danger mt-5">Email Salah</div>
-    <?php
+            $_SESSION['message'] = 'Berhasil Login';
+            header("location:../index.php");
+        } else { ?>
+            <div class="alert alert-danger mt-5">Password Salah</div>
+        <?php
+        }
+        
     }
-    
-}
-else{ ?>
-    <div class="alert alert-danger mt-5">Email Salah</div>
-<?php
+    else{ ?>
+        <div class="alert alert-danger mt-5">Email Salah</div>
+    <?php    
+    }
 }
 ?>
